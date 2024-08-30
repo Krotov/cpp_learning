fdb66dbd08d7d581ecee6b4f4afd682ab57fb77b
Anatolii Krotov <anatolii.krotov@gmail.com>
2024-08-30 19:13:18 +0200

    Added home_task_1

---
+++ home_task_1/home_task_1.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*
+    *  1) Вывести на экран консоли надпись:
+    */
+    cout << "\"To be\n\n";
+    cout << "\tor not\n\n";
+    cout << "\t\tto be...\"\n\n";
+    cout << "\t\t\t/ Shakespeare /\n\n";
+
+    /*
+    *  2) Вывести на экран консоли надпись 5 раз лесенкой:
+    */
+    cout << "I love C++!\n\n";
+    cout << "\tI love C++!\n\n";
+    cout << "\t\tI love C++!\n\n";
+    cout << "\t\t\tI love C++!\n\n";
+    cout << "\t\t\t\tI love C++!\n\n";
+
+    /*
+    *  6) Вывести свое имя на экран в виде символов. Например, звездочками.
+    */
+    cout << "*****   ***   *     ***  *  *\n";
+    cout << "  *    *   *  *      *   * *\n";
+    cout << "  *    *   *  *      *   **\n";
+    cout << "  *    *   *  *      *   * *\n";
+    cout << "  *     ***   ****  ***  *  *\n\n";
+
+    /*
+    *  7) Вывести на экран созвездие большой медведицы.
+    */
+
+    cout << "*\n";
+    cout << "        *\n\n";
+    cout << "             *\n\n";
+    cout << "                 *\n";
+    cout << "                              *\n\n";
+    cout << "                *\n";
+    cout << "                          *\n";
+}
